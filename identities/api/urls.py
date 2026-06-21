c1519a1dbcad369ae5a445b0a5e9714d8f89d4fc
Daniela Brailovsky <dbrailo@gmail.com>
2026-06-20 21:38:59 -0400

    GET context profile id endpoint implemented

--- identities/api/urls.py
+++ identities/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from identities.api.views.context import ContextListAPIView
-from identities.api.views.context_profile import ContextProfileListCreateAPIView
+from identities.api.views.context_profile import (ContextProfileListCreateAPIView, ContextProfileDetailAPIView)
 
 urlpatterns = [
     path('contexts/', ContextListAPIView.as_view(), name='context-list'),
     path('context-profiles/', ContextProfileListCreateAPIView.as_view(), name='context-profile-list-create'),
+    path('context-profiles/<int:pk>/', ContextProfileDetailAPIView.as_view(), name='context-profile-detail'),
 ]
--- identities/api/views/context_profile.py
+++ identities/api/views/context_profile.py
@@ -1,8 +1,8 @@
-from rest_framework.generics import ListAPIView
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
 from identities.models import ContextProfile
 from identities.api.serializers.context_profile import ContextProfileSerializer
+from django.shortcuts import get_object_or_404
 
 class ContextProfileListCreateAPIView(ListCreateAPIView):
     serializer_class = ContextProfileSerializer
@@ -13,3 +13,10 @@ class ContextProfileListCreateAPIView(ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(account=self.request.user) #security enforcement
+
+class ContextProfileDetailAPIView(RetrieveAPIView):
+    serializer_class = ContextProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return ContextProfile.objects.filter(account=self.request.user)
