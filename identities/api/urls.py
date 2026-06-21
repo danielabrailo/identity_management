1043514b8283b13e39b6dcb718edfa6ce24387fe
Daniela Brailovsky <dbrailo@gmail.com>
2026-06-20 21:31:16 -0400

    POST context profile endpoint implemented

--- identities/api/urls.py
+++ identities/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from identities.api.views.context import ContextListAPIView
-from identities.api.views.context_profile import ContextProfileListAPIView
+from identities.api.views.context_profile import ContextProfileListCreateAPIView
 
 urlpatterns = [
     path('contexts/', ContextListAPIView.as_view(), name='context-list'),
-    path('context-profiles/', ContextProfileListAPIView.as_view()),
+    path('context-profiles/', ContextProfileListCreateAPIView.as_view(), name='context-profile-list-create'),
 ]
--- identities/api/views/context_profile.py
+++ identities/api/views/context_profile.py
@@ -1,12 +1,15 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from identities.models import ContextProfile
 from identities.api.serializers.context_profile import ContextProfileSerializer
 
-
-class ContextProfileListAPIView(ListAPIView):
+class ContextProfileListCreateAPIView(ListCreateAPIView):
     serializer_class = ContextProfileSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return ContextProfile.objects.filter(account=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(account=self.request.user) #security enforcement
