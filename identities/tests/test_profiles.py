7574fe4379b10fec3decf36f7f66874a5b3c5dcc
Daniela Brailovsky <dbrailo@gmail.com>
2026-07-24 16:12:58 -0400

    added a test to confirm authenticated user retrieves only their own profiles

--- identities/tests/test_profiles.py
+++ identities/tests/test_profiles.py
@@ -11,16 +11,20 @@ from identities.models import (
 class ContextProfileTests(APITestCase):
     #create test data
     def setUp(self):
-        #create test user
+        #create test users
         self.user = User.objects.create_user(
             username="john",
+            password="testpassword123"
+        )
+        self.user2 = User.objects.create_user(
+            username="marie",
             password="testpassword123"
         )
         #get or create create "Professional" test context
         self.context, _ = Context.objects.get_or_create(
             name="Professional"
         )
-        #make user authenticated
+        #make users authenticated
         self.client.force_authenticate(user=self.user)
 
     #test: create a context profile
@@ -66,6 +70,38 @@ class ContextProfileTests(APITestCase):
             response.data[0]["display_name"],
             "John Doe"
         )
+
+    #test: retrieve all context profiles from authenticated user
+    def test_retrieve_authenticated_user_context_profiles(self):
+        #create profiles from different users
+        ContextProfile.objects.create(
+            account=self.user,
+            context=self.context,
+            display_name="John Doe"
+        )
+        ContextProfile.objects.create(
+            account=self.user2,
+            context=self.context,
+            display_name="Marie"
+        )
+        #generate url
+        url = reverse("context-profile-list-create")
+        #call endpoint
+        response = self.client.get(url)
+        #assertions
+        print(response.data)
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(
+            response.data[0]["display_name"],
+            "John Doe"
+        )
+        #assert that Marie is not among the response
+        display_names = [profile["display_name"] for profile in response.data]
+        self.assertNotIn("Marie", display_names)
 
     #test: retrieve user's single profile
     def test_retrieve_user_context_profile(self):
