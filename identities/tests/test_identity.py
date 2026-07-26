5cde7274fe63661e9ae5c6ad387820f11ea9f605
Daniela Brailovsky <dbrailo@gmail.com>
2026-07-25 20:37:34 -0400

    Added identity evaluation tests

--- identities/tests/test_identity.py
+++ identities/tests/test_identity.py
@@ -62,3 +62,62 @@ class ContextProfileTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["display_name"], "John Doe")
         self.assertIsNone(response.data["email"])
+
+    #test profile not found
+    def test_profile_not_found(self):
+        #create requester
+        requester = RequesterType.objects.create(
+            name="HR"
+        )
+        #create policy
+        policy = Policy.objects.create(
+            account=self.user,
+            context=self.context,
+            requester_type=requester,
+            can_view_display_name=True,
+            can_view_email=False
+        )
+        #use django's rever to generate URL based on view name
+        url = reverse("identity-evaluation")
+        #call endpoint
+        response = self.client.post(
+            url,
+           {
+                "target_user_id": self.user.id,
+                "context_id": self.context.id,
+                "requester_type_id": requester.id
+            },
+            format="json"
+        )
+        #assertions
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.data["error"], "No profile found")
+
+    #test policy not found
+    def test_policy_not_found(self):
+        #create requester
+        requester = RequesterType.objects.create(
+            name="HR"
+        )
+        #create profile
+        profile = ContextProfile.objects.create(
+            account=self.user,
+            context=self.context,
+            display_name="John Doe",
+            email="john@test.com"
+        )
+        #use django's rever to generate URL based on view name
+        url = reverse("identity-evaluation")
+        #call endpoint
+        response = self.client.post(
+            url,
+            {
+                "target_user_id": self.user.id,
+                "context_id": self.context.id,
+                "requester_type_id": requester.id
+            },
+            format="json"
+        )
+        #assertions
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.data["error"], "No policy found")
