8bea7debd8f25f13de22ff030f2903c7a7f46877
Daniela Brailovsky <dbrailo@gmail.com>
2026-07-25 21:50:38 -0400

    added uniqueness constraint validation to policy

--- identities/api/serializers/policy.py
+++ identities/api/serializers/policy.py
@@ -26,8 +26,37 @@ class PolicySerializer(serializers.ModelSerializer):
         read_only_fields = ['id']
 
     def validate(self, attrs):
+        #account validation
         if 'account' in self.initial_data:
             raise serializers.ValidationError({
                 "account": "This field cannot be set manually."
             })
+        #uniqueness constriant validation
+        user = self.context["request"].user
+        #get context
+        context = attrs.get(
+            "context",
+            self.instance.context if self.instance else None
+        )
+        #get requester type
+        requester_type = attrs.get(
+            "requester_type",
+            self.instance.requester_type if self.instance else None
+        )
+        if context and requester_type:
+            queryset = Policy.objects.filter(
+                account=user,
+                context=context,
+                requester_type=requester_type
+            )
+            #exclude current object during updates
+            if self.instance:
+                queryset = queryset.exclude(
+                    pk=self.instance.pk
+                )
+            #if it already exists
+            if queryset.exists():
+                raise serializers.ValidationError(
+                    "A policy already exists for this context and requester type."
+                )
         return attrs
